83fb77afe9ac73639f7c5913af39af31bba7c751
BrennoPh <Brenno.dossantos@dcomp.ufs.br>
2025-08-17 22:12:45 -0300

    Subindo estrutura do projeto
    Subindo o projeto criado como Maven. Já pré-modelado no padrão MVVM.

---
+++ SiteDeEventos/src/main/java/com/example/SiteDeEventos/SiteDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.example.SiteDeEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteDeEventosApplication.class, args);
+	}
+
+}
---
+++ SiteDeEventos/src/test/java/com/example/SiteDeEventos/SiteDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SiteDeEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
