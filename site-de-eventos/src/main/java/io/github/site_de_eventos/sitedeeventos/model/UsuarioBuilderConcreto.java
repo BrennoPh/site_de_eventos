df948fec29226112d91851ac94ca835354e70a07
Sibele Cruz <sibele.cruz@dcomp.ufs.br>
2025-08-25 12:14:05 -0300

    Update UsuarioBuilderConcreto.java

--- site-de-eventos/src/main/java/io/github/site_de_eventos/sitedeeventos/model/UsuarioBuilderConcreto.java
+++ site-de-eventos/src/main/java/io/github/site_de_eventos/sitedeeventos/model/UsuarioBuilderConcreto.java
@@ -21,55 +21,56 @@ public class UsuarioBuilderConcreto implements IUsuarioBuilder {
     /**
      * Construtor que inicializa um novo objeto {@link Usuario} para ser configurado.
      */
+    @Override
     public UsuarioBuilderConcreto() {
         this.usuario = new Usuario();
     }
-
     /**
      * Define o ID do usuário.
      * @param idUsuario (int) O ID a ser definido.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder idUsuario(int idUsuario) {
         this.usuario.setIdUsuario(idUsuario);
         return this;
     }
-
     /**
      * Define o nome do usuário.
      * @param nome (String) O nome a ser definido.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder nome(String nome) {
         this.usuario.setNome(nome);
         return this;
     }
-
     /**
      * Define o e-mail do usuário.
      * @param email (String) O e-mail a ser definido.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder email(String email) {
         this.usuario.setEmail(email);
         return this;
     }
-
     /**
      * Define o CPF do usuário.
      * @param cpf (String) O CPF a ser definido.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder cpf(String cpf) {
         this.usuario.setCpf(cpf);
         return this;
     }
-
     /**
      * Define o telefone do usuário.
      * @param telefone (String) O telefone a ser definido.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder telefone(String telefone) {
         this.usuario.setTelefone(telefone);
         return this;
@@ -80,41 +81,46 @@ public class UsuarioBuilderConcreto implements IUsuarioBuilder {
      * @param cidade (String) A cidade a ser definida.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder cidade(String cidade) {
         this.usuario.setCidade(cidade);
         return this;
     }
-
     /**
      * Define o endereço do usuário.
      * @param endereco (String) O endereço a ser definido.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder endereco(String endereco) {
         this.usuario.setEndereco(endereco);
         return this;
     }
-
     /**
      * Define a data de nascimento do usuário.
      * @param dataNascimento (LocalDateTime) A data a ser definida.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder dataNascimento(LocalDateTime dataNascimento) {
         this.usuario.setDataNascimento(dataNascimento);
         return this;
     }
-
     /**
      * Adiciona um pedido à lista de pedidos do usuário.
      * @param pedidos ({@link Pedido}) O pedido a ser adicionado.
      * @return A própria instância do builder para chamadas encadeadas.
      */
+    @Override
     public IUsuarioBuilder pedidos(Pedido pedidos) {
         this.usuario.adicionarPedido(pedidos);
         return this;
     }
-
+    @Override	
+	public IUsuarioBuilder pegaPedidos(List<Pedido> pedidos){
+    	this.usuario.setPedidos(pedidos);
+    	return this;
+	}
     /**
      * Constrói e retorna o objeto {@link Usuario} final.
      * @return O objeto {@link Usuario} configurado.
