82924ff8968cfe0226e0ab9aa287f5bf6d9016e7
Silas Santos <silas.dasilva@dcomp.ufs.br>
2025-08-25 11:04:40 -0300

    Delete site-de-eventos/src/test/java/io/github/site_de_eventos/sitedeeventos/service/strategy directory

--- site-de-eventos/src/test/java/io/github/site_de_eventos/sitedeeventos/service/strategy/ICalculoPrecoPedidoStrategy.java
+++
@@ -1,7 +0,0 @@
-package io.github.site_de_eventos.sitedeeventos.service.strategy;
-
-import io.github.site_de_eventos.sitedeeventos.model.Pedido;
-
-public interface ICalculoPrecoPedidoStrategy {
-	double calcularPreco(Pedido pedido);
-}
